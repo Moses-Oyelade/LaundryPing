878e88fd2aa4f9d2972b4f64a7ca2cee74cf49d3
Moses-Oyelade <oyelademoses@gmail.com>
2025-11-02 05:19:53 +0100

    update done button

--- frontend/src/components/MachineDashboard.jsx
+++ frontend/src/components/MachineDashboard.jsx
@@ -136,12 +136,13 @@ export default function MachineDashboard() {
           </div>
         ))}
       </div>
-      <button
-          onClick={handleAdd}
+        <a href='/home'>
+        <button
           className="bg-green-600 hover:bg-green-700 text-white px-4 py-2 rounded-md transition w-12 sm:w-auto"
         >
           Done
         </button>
+        </a>
     </div>
   );
 }
